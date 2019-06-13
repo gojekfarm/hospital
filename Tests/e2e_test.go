107e8560512108082ba8dc7532a3d62220698979
jainammm <jainam.iitrpr@gmail.com>
2019-06-13 16:37:43 +0530

    Remove old test
    Co-authored-by: Dilip Sharma <dilipsharma640@gmail.com>

--- Tests/e2e_test.go
+++ Tests/e2e_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestReceptionFiring(t *testing.T) {
-	storage.Connect()
+	storage.Initialize()
 	var jsonStr = []byte(`{"receiver":"pepper","status":"firing","alerts":[{"status":"firing","labels":{"alertname":"test","backend":"localnodes","instance":"test","job":"haproxy","severity":"page"},"annotations":{"summary":"Current queue is greater than 100"},"startsAt":"test","endsAt":"0001-01-01T00:00:00Z","generatorURL":"http://Dilips-MacBook-Pro.local:9090/graph?g0.expr=haproxy_backend_current_queue%7Bbackend%3D%22localnodes%22%2Cinstance%3D%22localhost%3A9101%22%2Cjob%3D%22haproxy%22%7D+%3E+100\u0026g0.tab=1"}],"groupLabels":{},"commonLabels":{"alertname":"queue_exceed","backend":"localnodes","instance":"localhost:9101","job":"haproxy","severity":"page"},"commonAnnotations":{"summary":"Current queue is greater than 100"},"externalURL":"http://Dilips-MacBook-Pro.local:9093","version":"4","groupKey":"{}:{}"}
 	`)
 	req, err := http.NewRequest("POST", "/reception", bytes.NewBuffer(jsonStr))
@@ -28,9 +28,12 @@ func TestReceptionFiring(t *testing.T) {
 
 	assert.Equal(t, 200, rr.Code, "OK response is expected")
 
-	var resp respstr
+	var resp receptionResponse
 	body, _ := ioutil.ReadAll(rr.Body)
-	json.Unmarshal(body, &resp)
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		t.Fatal(err)
+	}
 	expected := "firing"
 	if resp.Status != expected {
 		t.Errorf("alert send failed")
@@ -38,7 +41,7 @@ func TestReceptionFiring(t *testing.T) {
 }
 
 func TestReceptionResolved(t *testing.T) {
-	storage.Connect()
+	storage.Initialize()
 	var jsonStr = []byte(`{"receiver":"pepper","status":"firing","alerts":[{"status":"resolved","labels":{"alertname":"test","backend":"localnodes","instance":"test","job":"haproxy","severity":"page"},"annotations":{"summary":"Current queue is greater than 100"},"startsAt":"test","endsAt":"0001-01-01T00:00:00Z","generatorURL":"http://Dilips-MacBook-Pro.local:9090/graph?g0.expr=haproxy_backend_current_queue%7Bbackend%3D%22localnodes%22%2Cinstance%3D%22localhost%3A9101%22%2Cjob%3D%22haproxy%22%7D+%3E+100\u0026g0.tab=1"}],"groupLabels":{},"commonLabels":{"alertname":"queue_exceed","backend":"localnodes","instance":"localhost:9101","job":"haproxy","severity":"page"},"commonAnnotations":{"summary":"Current queue is greater than 100"},"externalURL":"http://Dilips-MacBook-Pro.local:9093","version":"4","groupKey":"{}:{}"}
 	`)
 	req, err := http.NewRequest("POST", "/reception", bytes.NewBuffer(jsonStr))
@@ -52,15 +55,18 @@ func TestReceptionResolved(t *testing.T) {
 
 	assert.Equal(t, 200, rr.Code, "OK response is expected")
 
-	var resp respstr
+	var resp receptionResponse
 	body, _ := ioutil.ReadAll(rr.Body)
-	json.Unmarshal(body, &resp)
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		t.Fatal(err)
+	}
 	expected := "resolved"
 	if resp.Status != expected {
 		t.Errorf("alert send failed")
 	}
 }
 
-type respstr struct {
+type receptionResponse struct {
 	Status string `json: "status"`
 }
