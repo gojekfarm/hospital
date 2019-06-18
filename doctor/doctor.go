e6b8b152e0b3a080dba812ff862dc5fe9f2044d9
jainammm <jainam.iitrpr@gmail.com>
2019-06-18 18:16:23 +0530

    Handle no script found from mapping table
    Co-authored-by: Dilip Sharma <dilipsharma640@gmail.com>

--- doctor/doctor.go
+++ doctor/doctor.go
@@ -6,6 +6,8 @@ import (
 
 func resolveAlert(alertID int, alertName, jobName string) string {
 	script := storage.GetScript(alertName)
-	storage.InsertOperation(alertID, jobName, script, "firing")
+	if script != "no script" {
+		storage.InsertOperation(alertID, jobName, script, "firing")
+	}
 	return "done"
 }
