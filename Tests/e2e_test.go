555092a4fbd584b1b1d49d64a3a7595d6c2e87f5
dilip640 <dilipsharma640@gmail.com>
2019-06-25 12:56:56 +0530

    Merge conflicts

--- Tests/e2e_test.go
+++ Tests/e2e_test.go
@@ -19,7 +19,6 @@ import (
 
 func TestReceptionFiring(t *testing.T) {
 	storage.Initialize()
-<<<<<<< HEAD
 
 	lable := reception.Label{Alertname: "test", Instance: "test", Job: "receptiontest"}
 	alert := reception.Alert{Status: "firing", StartsAt: "test", Labels: lable}
@@ -30,12 +29,6 @@ func TestReceptionFiring(t *testing.T) {
 		panic(err)
 	}
 
-=======
-	db := storage.ReturnDbInstance()
-
-	var jsonStr = []byte(`{"receiver":"pepper","status":"firing","alerts":[{"status":"firing","labels":{"alertname":"test","backend":"localnodes","instance":"test","job":"receptiontest","severity":"page"},"annotations":{"summary":"Current queue is greater than 100"},"startsAt":"test","endsAt":"0001-01-01T00:00:00Z","generatorURL":"http://Dilips-MacBook-Pro.local:9090/graph?g0.expr=haproxy_backend_current_queue%7Bbackend%3D%22localnodes%22%2Cinstance%3D%22localhost%3A9101%22%2Cjob%3D%22haproxy%22%7D+%3E+100\u0026g0.tab=1"}],"groupLabels":{},"commonLabels":{"alertname":"queue_exceed","backend":"localnodes","instance":"localhost:9101","job":"haproxy","severity":"page"},"commonAnnotations":{"summary":"Current queue is greater than 100"},"externalURL":"http://Dilips-MacBook-Pro.local:9093","version":"4","groupKey":"{}:{}"}
-	`)
->>>>>>> 9889765a980c77d6c5b8c5642d7e03a7e41e26c2
 	req, err := http.NewRequest("POST", "/reception", bytes.NewBuffer(jsonStr))
 	if err != nil {
 		t.Fatal(err)
